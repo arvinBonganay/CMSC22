f22bdfc324772003bee771638940b654a20bcf76
arvinBonganay <arvinb450@gmail.com>
2016-10-07 17:39:20 +0800

    Inheritance

---
+++ Circle.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.lang.*;
+
+public class Circle extends Shape{
+	private double radius = 1.0;
+
+	public Circle(){
+		radius = 1.0;
+	} 
+
+	public Circle(double radius){
+		if(radius <= 0)
+			throw new IllegalArgumentException("Length of radius should be greater than 0");
+		this.radius = radius;
+	}
+
+	public Circle(double radius, String color, boolean filled){
+		super(color, filled);
+	}
+
+	public double getRadius(){
+		return radius;
+	}
+
+	public void setRadius(double radius){
+		if(radius <= 0)
+			throw new IllegalArgumentException("Length of radius should be greater than 0");
+		this.radius = radius;
+	}
+
+	public double getArea(){
+		double area = radius * radius * Math.PI;
+		return area;
+	}
+
+	public double getPerimeter(){
+		double perimeter = 2 * Math.PI * radius;
+		return perimeter;
+	}
+
+	public String toString(){
+		String x = String.format("A Circle with radius = %.3f, which is a subclass of ", radius) + super.toString();
+		return x;
+	}
+}
